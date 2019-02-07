42bbbe2e85ce89fcf816cd6400ef8c35943ee4cd
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-02-07 10:48:30 -0500

    Update ks-setup-step1.sh

--- ks-setup-step1.sh
+++ ks-setup-step1.sh
@@ -1,7 +1,11 @@
 chmod +x ./*.sh &>> ./setup-step1.out
+echo "1...\n" >> ./setup-step1.out
 ./turn_off_selinux.sh &>> ./setup-step1.out
+echo "12...\n" >> ./setup-step1.out
 ./turn_off_swap.sh &>> ./setup-step1.out
-./fstab.sh &>> ./setup-step1.out
+echo "123...\n" >> ./setup-step1.out
 ./set_up_bridging.sh &>> ./setup-step1.out
+echo "1234\n" >> ./setup-step1.out
 cp ./kubernetes.repo /etc/yum.repos.d/kubernetes.repo &>> ./setup-step1.out
+echo "Rebooting now...\n" >> ./setup-step1.out
 reboot
