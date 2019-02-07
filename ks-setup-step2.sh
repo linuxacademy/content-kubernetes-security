65505a0c4fd3ec9f9ee060f2cdc718786c2f2427
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-02-07 10:52:36 -0500

    Update ks-setup-step2.sh

--- ks-setup-step2.sh
+++ ks-setup-step2.sh
@@ -1,6 +1,11 @@
 chmod +x *.sh &>> ./setup-step2.out 
+echo "1...\n" >> ./setup-step2.out
 ./yum_install_docker_ce.sh &>> ./setup-step2.out
+echo "12...\n" >> ./setup-step2.out
 ./yum_install_kube.sh &>> ./setup-step2.out
+echo "123...\n" >> ./setup-step2.out
 ./enable_start_docker_kube.sh &>> ./setup-step2.out
+echo "1234...\n" >> ./setup-step2.out
 ./set_cgroup_driver.sh &>> ./setup-step2.out
+echo "Complete.\n" >> ./setup-step2.out
 
