d025a3838980b03b2ded9c987a9c23867b331cb8
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-10-22 14:54:03 -0400

    Update ks-setup-step2.sh

--- ks-setup-step2.sh
+++ ks-setup-step2.sh
@@ -5,4 +5,6 @@ echo "12...\n" >> ./setup-step2.out
 ./yum_install_kube.sh &>> ./setup-step2.out
 echo "123...\n" >> ./setup-step2.out
 ./enable_start_docker_kube.sh &>> ./setup-step2.out
+echo "1234...\n" >> ./setup-step2.out
+./set_up_bridging.sh &>> ./setup-step2.out
 echo "Complete.\n" >> ./setup-step2.out
