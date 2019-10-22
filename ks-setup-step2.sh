b5e9a603bf3cd023e89cf18fc7d24735c9260549
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-10-22 12:31:39 -0400

    Update ks-setup-step2.sh
    Eliminated cgroupfs driver setup as Kubernetes now recommends system

--- ks-setup-step2.sh
+++ ks-setup-step2.sh
@@ -5,7 +5,4 @@ echo "12...\n" >> ./setup-step2.out
 ./yum_install_kube.sh &>> ./setup-step2.out
 echo "123...\n" >> ./setup-step2.out
 ./enable_start_docker_kube.sh &>> ./setup-step2.out
-echo "1234...\n" >> ./setup-step2.out
-./set_cgroup_driver.sh &>> ./setup-step2.out
 echo "Complete.\n" >> ./setup-step2.out
-
